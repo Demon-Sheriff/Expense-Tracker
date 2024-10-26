f7d5e3a138182cfa2fecf3ed1447fa5f0054a928
DemonSheriff <anantshukla404@gmail.com>
2024-10-26 12:07:31 +0530

    added async handling operations for delete and cancel from user

--- lib/widgets/expense_list/expense_list.dart
+++ lib/widgets/expense_list/expense_list.dart
@@ -1,20 +1,69 @@
 import 'package:expense_tracker_app/models/expense.dart';
-import 'package:expense_tracker_app/models/expense_category.dart';
 import 'package:expense_tracker_app/widgets/expense_list/expense_item.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class ExpenseList extends StatelessWidget {
   final List<Expense> expenseList;
-  const ExpenseList({super.key, required this.expenseList});
+  final Function(int) onRemoveItem;
+  const ExpenseList(
+      {super.key, required this.expenseList, required this.onRemoveItem});
 
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
       itemCount: expenseList.length,
       itemBuilder: (context, index) {
-        return ExpenseItem(
-          expense: expenseList[index],
+        return Dismissible(
+          key: ValueKey(expenseList[index]),
+          // onDismissed: (direction) {
+          //   // print('hi');
+          //   print(expenseList.length);
+          //   onRemoveItem(index);
+          // },
+          confirmDismiss: (direction) async {
+            // if the user swipes to the right.
+            if (direction == DismissDirection.endToStart) {
+              final bool confirmationToDelete = await showDialog(
+                context: context,
+                builder: (BuildContext context) {
+                  return AlertDialog(
+                    content: Text(
+                        'Are you sure you want to delete expense ${expenseList[index].title} ?'),
+                    actions: [
+                      TextButton(
+                        onPressed: () {
+                          Navigator.of(context).pop(false);
+                        },
+                        child: const Text('Cancel'),
+                      ),
+                      TextButton(
+                        onPressed: () {
+                          onRemoveItem(index);
+                          Navigator.of(context).pop(true);
+                        },
+                        child: const Text(
+                          'Delete',
+                          style: TextStyle(
+                            color: Colors.red,
+                          ),
+                        ),
+                      ),
+                    ],
+                  );
+                },
+              );
+              print(confirmationToDelete);
+              return confirmationToDelete;
+            }
+            return null;
+          },
+          child: InkWell(
+            // for click effect.
+            onTap: () {},
+            child: ExpenseItem(
+              expense: expenseList[index],
+            ),
+          ),
         );
       },
     );
